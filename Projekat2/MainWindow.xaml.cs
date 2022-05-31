817ae594dd0fa6468322f2b70ba5e446f6360590
Marko Đurđević <markodjurdjevic@uns.ac.rs>
2022-05-31 20:16:46 +0200

    ucitavanje modela i viewport

--- Projekat2/MainWindow.xaml.cs
+++ Projekat2/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Projekat2.Utils;
 
 namespace Projekat2
 {
@@ -20,9 +9,90 @@ namespace Projekat2
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Fields
+
+        private Point _start = new Point();
+        private Point _diffOffset = new Point();
+        private readonly int _zoomInMax = 50;
+        private readonly int _zoomOutMax = 5;
+        private int _zoomCurent = 1;
+
+        private Grid _grid;
+        private bool _isLoaded = false;
+
+        #endregion
+
         public MainWindow()
         {
             InitializeComponent();
+            _grid = new Grid();
         }
+
+        #region ZoomPan
+
+        private void ViewPort_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            ViewPort.CaptureMouse();
+            _start = e.GetPosition(this);
+            _diffOffset.X = Translate.OffsetX;
+            _diffOffset.Y = Translate.OffsetY;
+        }
+
+        private void ViewPort_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            ViewPort.ReleaseMouseCapture();
+        }
+
+        private void ViewPort_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (ViewPort.IsMouseCaptured)
+            {
+                Point end = e.GetPosition(this);
+                double offsetX = end.X - _start.X;
+                double offsetY = end.Y - _start.Y;
+                double w = this.Width;
+                double h = this.Height;
+                double translateX = (offsetX * 100) / w;
+                double translateY = -(offsetY * 100) / h;
+                Translate.OffsetX = _diffOffset.X + (translateX / (100 * Scale.ScaleX));
+                Translate.OffsetY = _diffOffset.Y + (translateY / (100 * Scale.ScaleX));
+            }
+        }
+
+        private void ViewPort_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            Point p = e.MouseDevice.GetPosition(this);
+            double scaleX = 1;
+            double scaleY = 1;
+            if (e.Delta > 0 && _zoomCurent < _zoomInMax)
+            {
+                scaleX = Scale.ScaleX + 0.1;
+                scaleY = Scale.ScaleY + 0.1;
+                _zoomCurent++;
+                Scale.ScaleX = scaleX;
+                Scale.ScaleY = scaleY;
+            }
+            else if (e.Delta <= 0 && _zoomCurent > -_zoomOutMax)
+            {
+                scaleX = Scale.ScaleX - 0.1;
+                scaleY = Scale.ScaleY - 0.1;
+                _zoomCurent--;
+                Scale.ScaleX = scaleX;
+                Scale.ScaleY = scaleY;
+            }
+        }
+
+        #endregion
+
+        #region LoadModel
+
+        private void LoadModel_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_isLoaded) _grid.LoadModel();
+            _isLoaded = true;
+            int a = 5;
+        }
+
+        #endregion
     }
 }
---
+++ Projekat2/Utils/Grid.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using Projekat.Model;
+
+namespace Projekat2.Utils
+{
+    public class Grid
+    {
+        private readonly double _maxLat = 45.277031;
+        private readonly double _minLat = 45.2325;
+        private readonly double _maxLon = 19.894459;
+        private readonly double _minLon = 19.793909;
+
+        private double _noviX = 0, _noviY = 0;
+        private Dictionary<string, double> Coordinates { get; } = new Dictionary<string, double>(4)
+        {
+            {"maxLat", double.MinValue}, {"maxLon", double.MinValue},
+            {"minLat", double.MaxValue}, {"minLon", double.MaxValue}
+        };
+
+        private readonly string _path = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.FullName + "\\Resources", "Geographic.xml");
+
+        public List<SubstationEntity> SubstationEntities { get; } = new List<SubstationEntity>(17);
+        public List<NodeEntity> NodeEntities { get; } = new List<NodeEntity>(795);
+        public List<SwitchEntity> SwitchEntities { get; } = new List<SwitchEntity>(1233);
+        public List<LineEntity> LineEntities { get; } = new List<LineEntity>(888);
+
+        public Grid() { }
+
+        public void LoadModel()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(_path);
+
+            Substations(xmlDoc);
+            Nodes(xmlDoc);
+            Switches(xmlDoc);
+            Routes(xmlDoc);
+        }
+
+        private void Substations(XmlDocument xmlDoc)
+        {
+            XmlNodeList nodeList = xmlDoc.DocumentElement.SelectNodes("/NetworkModel/Substations/SubstationEntity");
+            foreach (XmlNode node in nodeList)
+            {
+                SubstationEntity sub = new SubstationEntity()
+                {
+                    Id = long.Parse(node.SelectSingleNode("Id").InnerText),
+                    Name = node.SelectSingleNode("Name").InnerText,
+                    X = double.Parse(node.SelectSingleNode("X").InnerText),
+                    Y = double.Parse(node.SelectSingleNode("Y").InnerText)
+                };
+
+                ToLatLon(sub.X, sub.Y, 34, out _noviX, out _noviY);
+                if (!CheckBounds()) continue;
+                sub.X = _noviX;
+                sub.Y = _noviY;
+                SubstationEntities.Add(sub);
+            }
+        }
+
+        private void Nodes(XmlDocument xmlDoc)
+        {
+            XmlNodeList nodeList = xmlDoc.DocumentElement.SelectNodes("/NetworkModel/Nodes/NodeEntity");
+            foreach (XmlNode node in nodeList)
+            {
+                NodeEntity nodeEntity = new NodeEntity()
+                {
+                    Id = long.Parse(node.SelectSingleNode("Id").InnerText),
+                    Name = node.SelectSingleNode("Name").InnerText,
+                    X = double.Parse(node.SelectSingleNode("X").InnerText),
+                    Y = double.Parse(node.SelectSingleNode("Y").InnerText)
+                };
+
+                ToLatLon(nodeEntity.X, nodeEntity.Y, 34, out _noviX, out _noviY);
+                if (!CheckBounds()) continue;
+                nodeEntity.X = _noviX;
+                nodeEntity.Y = _noviY;
+                NodeEntities.Add(nodeEntity);
+            }
+        }
+
+        private void Switches(XmlDocument xmlDoc)
+        {
+            XmlNodeList nodeList = xmlDoc.DocumentElement.SelectNodes("/NetworkModel/Switches/SwitchEntity");
+            foreach (XmlNode node in nodeList)
+            {
+                SwitchEntity switchobj = new SwitchEntity()
+                {
+                    Id = long.Parse(node.SelectSingleNode("Id").InnerText),
+                    Name = node.SelectSingleNode("Name").InnerText,
+                    X = double.Parse(node.SelectSingleNode("X").InnerText),
+                    Y = double.Parse(node.SelectSingleNode("Y").InnerText),
+                    Status = node.SelectSingleNode("Status").InnerText
+                };
+
+                ToLatLon(switchobj.X, switchobj.Y, 34, out _noviX, out _noviY);
+                if (!CheckBounds()) continue;
+                switchobj.X = _noviX;
+                switchobj.Y = _noviY;
+                SwitchEntities.Add(switchobj);
+            }
+        }
+
+        private void Routes(XmlDocument xmlDoc)
+        {
+            XmlNodeList nodeList = xmlDoc.DocumentElement.SelectNodes("/NetworkModel/Lines/LineEntity");
+            foreach (XmlNode node in nodeList)
+            {
+                bool skip = false;
+                LineEntity l = new LineEntity()
+                {
+                    Id = long.Parse(node.SelectSingleNode("Id").InnerText),
+                    Name = node.SelectSingleNode("Name").InnerText,
+                    IsUnderground = node.SelectSingleNode("IsUnderground").InnerText.Equals("true"),
+                    R = float.Parse(node.SelectSingleNode("R").InnerText),
+                    ConductorMaterial = node.SelectSingleNode("ConductorMaterial").InnerText,
+                    LineType = node.SelectSingleNode("LineType").InnerText,
+                    ThermalConstantHeat = long.Parse(node.SelectSingleNode("ThermalConstantHeat").InnerText),
+                    FirstEnd = long.Parse(node.SelectSingleNode("FirstEnd").InnerText),
+                    SecondEnd = long.Parse(node.SelectSingleNode("SecondEnd").InnerText),
+                    Vertices = new List<Point>()
+                };
+
+                foreach (XmlNode pointNode in node.ChildNodes[9].ChildNodes)
+                {
+                    Point p = new Point
+                    {
+                        X = double.Parse(pointNode.SelectSingleNode("X").InnerText),
+                        Y = double.Parse(pointNode.SelectSingleNode("Y").InnerText)
+                    };
+
+                    ToLatLon(p.X, p.Y, 34, out _noviX, out _noviY);
+                    if (!CheckBounds())
+                    {
+                        skip = true;
+                        break;
+                    }
+                    p.X = _noviX;
+                    p.Y = _noviY;
+                    l.Vertices.Add(p);
+                }
+
+                if (skip) continue;
+                LineEntities.Add(l);
+            }
+        }
+
+        private bool CheckBounds()
+        {
+            if (_noviX > Coordinates["maxLat"]) Coordinates["maxLat"] = _noviX;
+            if (_noviX < Coordinates["minLat"]) Coordinates["minLat"] = _noviX;
+            if (_noviY > Coordinates["maxLon"]) Coordinates["maxLon"] = _noviY;
+            if (_noviX < Coordinates["minLon"]) Coordinates["minLon"] = _noviY;
+            if (!(_noviX >= _minLat) || !(_noviX <= _maxLat)) return false;
+            return _noviY >= _minLon && _noviY <= _maxLon;
+        }
+
+        private void ToLatLon(double utmX, double utmY, int zoneUTM, out double latitude, out double longitude)
+        {
+            bool isNorthHemisphere = true;
+
+            var diflat = -0.00066286966871111111111111111111111111;
+            var diflon = -0.0003868060578;
+
+            var zone = zoneUTM;
+            var c_sa = 6378137.000000;
+            var c_sb = 6356752.314245;
+            var e2 = Math.Pow((Math.Pow(c_sa, 2) - Math.Pow(c_sb, 2)), 0.5) / c_sb;
+            var e2cuadrada = Math.Pow(e2, 2);
+            var c = Math.Pow(c_sa, 2) / c_sb;
+            var x = utmX - 500000;
+            var y = isNorthHemisphere ? utmY : utmY - 10000000;
+
+            var s = ((zone * 6.0) - 183.0);
+            var lat = y / (c_sa * 0.9996);
+            var v = (c / Math.Pow(1 + (e2cuadrada * Math.Pow(Math.Cos(lat), 2)), 0.5)) * 0.9996;
+            var a = x / v;
+            var a1 = Math.Sin(2 * lat);
+            var a2 = a1 * Math.Pow((Math.Cos(lat)), 2);
+            var j2 = lat + (a1 / 2.0);
+            var j4 = ((3 * j2) + a2) / 4.0;
+            var j6 = ((5 * j4) + Math.Pow(a2 * (Math.Cos(lat)), 2)) / 3.0;
+            var alfa = (3.0 / 4.0) * e2cuadrada;
+            var beta = (5.0 / 3.0) * Math.Pow(alfa, 2);
+            var gama = (35.0 / 27.0) * Math.Pow(alfa, 3);
+            var bm = 0.9996 * c * (lat - alfa * j2 + beta * j4 - gama * j6);
+            var b = (y - bm) / v;
+            var epsi = ((e2cuadrada * Math.Pow(a, 2)) / 2.0) * Math.Pow((Math.Cos(lat)), 2);
+            var eps = a * (1 - (epsi / 3.0));
+            var nab = (b * (1 - epsi)) + lat;
+            var senoheps = (Math.Exp(eps) - Math.Exp(-eps)) / 2.0;
+            var delt = Math.Atan(senoheps / (Math.Cos(nab)));
+            var tao = Math.Atan(Math.Cos(delt) * Math.Tan(nab));
+
+            longitude = ((delt * (180.0 / Math.PI)) + s) + diflon;
+            latitude = ((lat + (1 + e2cuadrada * Math.Pow(Math.Cos(lat), 2) - (3.0 / 2.0) * e2cuadrada * Math.Sin(lat) * Math.Cos(lat) * (tao - lat)) * (tao - lat)) * (180.0 / Math.PI)) + diflat;
+        }
+    }
+}
